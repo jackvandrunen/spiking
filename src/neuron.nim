9badfcf6220e72acb3296a55c6474433011163a1
Jack VanDrunen <jackvandrunen@gmail.com>
2023-03-22 13:12:42 -0700

    initial commit

---
+++ src/neuron.nim
@@ -0,0 +1,41 @@
+const
+    beta_e = 0.133
+    # sigma_e = 1.0
+    sigma_1 = 1.06  # sigma + 1
+    mu = 0.0005
+    alpha = 3.65
+
+
+type
+    Neuron* = tuple
+        V_n: float
+        I_n: float
+        V_n1: float  # V_{n-1}
+        spike: bool
+
+
+func checkSpike(V_n, V_n1, I_n: float): bool {.inline.} =
+    0 < V_n and V_n < alpha + I_n and V_n1 <= 0
+
+
+func f_alpha(V_n, V_n1, I_n: float): float =
+    if V_n <= 0:
+        alpha / (1.0 - V_n) + I_n
+    elif checkSpike(V_n, V_n1, I_n):
+        alpha + I_n
+    else:
+        -1.0
+
+
+func step(n: Neuron, I_ext: float): Neuron =
+    (
+        V_n: f_alpha(n.V_n, n.V_n1, n.I_n + beta_e * I_ext),
+        I_n: n.I_n - mu * (n.V_n + I_ext + sigma_1),
+        V_n1: n.V_n,
+        spike: checkSpike(n.V_n, n.V_n1, n.I_n + beta_e * I_ext)
+    )
+
+
+proc step*(neurons: var openArray[Neuron], I_exts: openArray[float]) =
+    for i, n in neurons:
+        neurons[i] = step(n, I_exts[i])
---
+++ src/synapse.nim
@@ -0,0 +1,41 @@
+import random
+
+import ./neuron
+
+
+const
+    gamma = 0.95  # TODO
+    base = 0.88
+    width = 0.24  # upper bound minus lower bound of the uniform random variable
+
+
+type
+    Synapse* = tuple
+        V_rp: float
+        w: float
+        g: float
+        I_syn: float
+
+
+proc step(s: Synapse, preSpike: bool, postV: float): Synapse =
+    (
+        V_rp: s.V_rp,
+        w: s.w,
+        g: (if preSpike:
+            gamma * s.g + (base + rand(width)) * s.g / s.w
+        else:
+            gamma * s.g),
+        I_syn: -s.g * (postV - s.V_rp)
+    )
+
+
+proc step*(synapses: var openArray[seq[Synapse]]; pre, post: var openArray[Neuron]; I_exts: openArray[float]): seq[float] =
+    # first call step() on pre, so can check if they are spiking
+    # post has not yet step'd, so can check V_n
+    # return the I_exts for the post layer, we assume that this is just the sum of I_syns
+    step(pre, I_exts)
+    result = newSeq[float](post.len)
+    for preIdx in 0 ..< synapses.len:
+        for postIdx in 0 ..< synapses[preIdx].len:
+            synapses[preIdx][postIdx] = step(synapses[preIdx][postIdx], pre[preIdx].spike, post[postIdx].V_n)
+            result[postIdx] += synapses[preIdx][postIdx].I_syn
